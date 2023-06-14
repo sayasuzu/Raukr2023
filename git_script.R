96caaaff8f9060270599020c30a597ea849e7b74
Sayaka Suzuki <ssayaka38@gmail.com>
2023-06-14 14:50:53 +0200

    add some function

--- git_script.R
+++ git_script.R
@@ -7,4 +7,17 @@ a + b -> c
 
 
 
+# Create a 100000 by 10 matrix with the numbers 1:1000000
+mat <- matrix(1:1000000, nrow = 100000, ncol = 10)
+print(mat)
+
+sum_row <- vector()
+for(i in 1:nrow(mat)){
+  sum_row[i] <- sum(mat[i,])
+}
+
+library(tictoc)
+tic()
+head(sum_row)
+toc()
 
